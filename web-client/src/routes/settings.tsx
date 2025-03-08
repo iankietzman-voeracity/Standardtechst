720a77e0a3e74f22d4e818de9a376a1d437295cb
iankietzman-voeracity <ian+voeracity@tallgrassschool.com>
2025-03-07 19:39:06 -0600

    feat: settings component schema, route protection, auth hook, useForm hook, markup

--- web-client/src/routes/settings.tsx
+++ web-client/src/routes/settings.tsx
@@ -6,6 +6,16 @@ import { useMutation } from "@tanstack/react-query";
 import { createFileRoute, redirect } from "@tanstack/react-router";
 import { z } from "zod";
 
+import { useAuth } from "../lib/AuthContext";
+import pb from "../lib/pb";
+
+const schema = z.object({
+  language: z.string(),
+  darkMode: z.boolean(),
+});
+
+type SettingsData = z.infer<typeof schema>;
+
 export const Route = createFileRoute("/settings")({
   beforeLoad: ({ context, location }) => {
     console.log(context);
@@ -19,26 +29,61 @@ export const Route = createFileRoute("/settings")({
       });
     }
   },
-  component: RouteComponent,
+  component: Settings,
 });
 
-function RouteComponent() {
-  return <div>
-    Settings
+function Settings() {
+  const {
+    isAuthenticated,
+    setIsAuthenticated,
+    isLoading,
+    userRecord,
+    token,
+    login,
+  } = useAuth();
 
-    <table>
-      <tbody>
-        <tr>
-          <td>Language</td>
-          <td><input /></td>
-        </tr>
-        <tr>
-          <td>Dark Mode</td>
-          <td><input /></td>
-        </tr>
-      </tbody>
-    </table>
+    const {
+      register,
+      handleSubmit,
+      formState: { errors, isSubmitting },
+    } = useForm<SettingsData>({
+      resolver: zodResolver(schema),
+    });
 
-  </div>;
+  return (
+    <div>
+      Settings
+
+      <Box maxWidth="360px" p="2">
+              <Form.Root onSubmit={handleSubmit(loginHandler)}>
+                <Form.Field name="language">
+                  <Form.Label>Language:</Form.Label>
+                  <Form.Control {...register("language")} type="text" required />
+                  {errors.language && <div>{errors.language.message}</div>}
+                </Form.Field>
+                <Form.Field name="password">
+                  <Form.Label>Dark Mode:</Form.Label>
+                  <Form.Control {...register("darkMode")} type="text" required />
+                  {errors.darkMode && <div>{errors.darkMode.message}</div>}
+                </Form.Field>
+      
+                <Form.Submit>{isSubmitting ? "Signing in" : "Sign in"}</Form.Submit>
+              </Form.Root>
+            </Box>
+      <table>
+        <tbody>
+          <tr>
+            <td>Language</td>
+            <td><input /></td>
+          </tr>
+          <tr>
+            <td>Dark Mode</td>
+            <td><input /></td>
+          </tr>
+        </tbody>
+      </table>
+
+    </div>
+  )
 
 }
